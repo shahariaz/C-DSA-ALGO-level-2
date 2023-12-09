e889a03d5c1bd2ea39ed9cf76a33e0e269dd2c00
shahariaz <shahariaz.info@gmail.com>
2023-12-09 22:18:26 +0600

    array and func

--- Array/array.cpp
+++ Array/array.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
+    // int arr[5] = {1, 2, 3, 4, 5};//static array of int beacouse we already declear the size of the array
+    int marks[100];
+    int n;
+    cout<<"Enter the no of Students"<<endl;
+    cin>>n;
+    //input
+    for (int i =0; i<n; i++){
+        cout<<"Enter the number of Students"<<(i+1)<<endl;
+        cin>>marks[i];
+    }
+    //Output 
+    for (int i =0; i<n; i++){
+        
+        cout<<"Marks of Students" <<i+1 <<"is"<<marks[i]<<endl;
+    }
+
     return 0;
 }
